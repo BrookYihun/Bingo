2f87522277fc149090f23085b0aac5e3027740ee
BrookYihun <brookyihun@gmail.com>
2024-11-05 09:11:20 +0300

    234

--- Bingo/settings.py
+++ Bingo/settings.py
@@ -161,6 +161,8 @@ STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 STATIC_ROOT = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Card, Game
 # Register your models here.
+
+admin.site.register(Game)
+admin.site.register(Card)
