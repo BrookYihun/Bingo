f5d7dc619ef70f2167e0e0c80a24022f695341f2
BrookYihun <brookyihun@gmail.com>
2024-12-25 21:17:26 +0300

    static

--- Bingo/settings.py
+++ Bingo/settings.py
@@ -158,7 +158,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'  # URL prefix for serving static files
+STATIC_URL = 'api/static/'  # URL prefix for serving static files
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory where collectstatic will store files
 
