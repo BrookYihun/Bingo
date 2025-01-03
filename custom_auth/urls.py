4a574b2f1ff62a4d2e6ef246c7be6710ea6dc9d4
BrookYihun <brookyihun@gmail.com>
2025-01-03 19:59:00 +0300

    ggf

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("send-otp/", SendOTPView.as_view(), name="send-otp"),
     path("verify-otp/", VerifyOTPView.as_view(), name="verify-otp"),
     path("verify-token/",verify_token,name="verify_token"),
-    path("get-blance/",get_balance,name="get_balance"),
+    path("get-balance/",get_balance,name="get_balance"),
 ]
