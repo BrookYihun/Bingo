d10661df9fb2026b940f736f765d7ceb82c0e4ee
BrookYihun <brookyihun@gmail.com>
2025-01-05 16:43:11 +0300

    bnb

--- Bingo/settings.py
+++ Bingo/settings.py
@@ -89,6 +89,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 ROOT_URLCONF = 'Bingo.urls'
