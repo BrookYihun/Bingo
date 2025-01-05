b50a196dfad2f2aafba90ad1a06a92b56bdeeb1c
BrookYihun <brookyihun@gmail.com>
2025-01-05 17:15:35 +0300

    ggg

--- Bingo/asgi.py
+++ Bingo/asgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 from game import routing
 
@@ -19,9 +20,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bingo.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+               routing.websocket_urlpatterns # WebSocket URL routing
+            )
         )
     ),
 })
