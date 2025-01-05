880286d5215df901dc68c29278324c2274bfa242
BrookYihun <brookyihun@gmail.com>
2025-01-05 17:00:00 +0300

    bb

--- Bingo/settings.py
+++ Bingo/settings.py
@@ -75,7 +75,7 @@ ASGI_APPLICATION = 'Bingo.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
