16b05fed814db6f806207565c1483ff5dd731594
BrookYihun <brookyihun@gmail.com>
2025-01-07 18:47:44 +0300

    vvv

--- custom_auth/middleware/session_expiry.py
+++ custom_auth/middleware/session_expiry.py
@@ -3,5 +3,9 @@ from django.http import JsonResponse
 
 class SessionExpiryMiddleware(MiddlewareMixin):
     def process_request(self, request):
+        
+        if request.path in ['/api/auth/login/', '/api/auth/register/', '/api/auth/send-otp/', '/api/auth/verify-otp/','/api/auth/verify-token/','/api/auth/refresh-session/']:  # Adjust the URLs accordingly
+            return None  # Skip the middleware logic
+        
         if not request.session.session_key:  # No session exists
             return JsonResponse({"error": "Session Expired"}, status=403)
