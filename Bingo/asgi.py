1347f1513de76f83bcf9526035945e260d785f4f
BrookYihun <brookyihun@gmail.com>
2025-01-13 20:15:56 +0300

    nfnfn

--- Bingo/asgi.py
+++ Bingo/asgi.py
@@ -1,20 +1,15 @@
-"""
-ASGI config for dallolbingo project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from game.middleware import JWTAuthMiddleware
 from game.routing import websocket_urlpatterns
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bingo.settings')
 
+# Define the ASGI application
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
@@ -24,6 +19,5 @@ application = ProtocolTypeRouter({
     ),
 })
 
-
-from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
+# Handle static files in ASGI
 application = ASGIStaticFilesHandler(application)
