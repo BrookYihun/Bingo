0d4f0f6ce883404f07c3a294775b8f512fd3c0fe
BrookYihun <brookyihun@gmail.com>
2025-01-14 13:59:13 +0300

    admin

--- custom_auth/admin.py
+++ custom_auth/admin.py
@@ -8,4 +8,23 @@ admin.site.register(AbstractUser)
 admin.site.register(User)
 admin.site.register(Cashier)
 
+from django.contrib.admin import AdminSite
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+
+class CustomAdminSite(AdminSite):
+    def has_permission(self, request):
+        try:
+            jwt_auth = JWTAuthentication()
+            user, _ = jwt_auth.authenticate(request)
+            if user and user.is_active and user.is_staff:
+                return True
+        except AuthenticationFailed:
+            return False
+        return False
+
+# Replace the default admin site
+custom_admin_site = CustomAdminSite()
+
+
 
