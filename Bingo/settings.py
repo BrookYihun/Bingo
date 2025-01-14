45b7e1942a950be7f1141c041652726b441b0839
BrookYihun <brookyihun@gmail.com>
2025-01-14 14:34:34 +0300

    sss

--- Bingo/settings.py
+++ Bingo/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
+    'django.contrib.sessions',
     # 'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
