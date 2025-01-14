1cbd7f20658e68b3aa58f9bdbd008c7f48b3e7b6
BrookYihun <brookyihun@gmail.com>
2025-01-14 14:37:08 +0300

    bmmb

--- Bingo/urls.py
+++ Bingo/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/admin/', admin.sits),
+    path('api/admin/', admin.site.urls),
     path('api/auth/', include('custom_auth.urls')),
     path('api/bingo/',include('game.urls')),
 ]
