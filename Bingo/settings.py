5e8c2f932fe1087c43d4d59e93b16da057248165
BrookYihun <brookyihun@gmail.com>
2025-01-14 13:52:56 +0300

    bbbb

--- Bingo/settings.py
+++ Bingo/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     # 'django.contrib.sessions.middleware.SessionMiddleware',  # Removed session middleware
     # 'custom_auth.middleware.session_expiry.SessionExpiryMiddleware',
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'game.middleware.JWTAuthenticationMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',  # Optional if you don't need CSRF protection
     # 'django.contrib.messages.middleware.MessageMiddleware',
