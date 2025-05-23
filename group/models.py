8888a779d0acbb76d77756ec14be35d92adfdef2
BrookYihun <brookyihun@gmail.com>
2025-05-23 18:51:22 +0300

    mnm

--- group/models.py
+++ group/models.py
@@ -1,37 +1,41 @@
-import uuid
 from django.db import models
-from custom_auth.models import AbstractUser
+from django.conf import settings
 
 class Group(models.Model):
-    GROUP_PRIVACY_CHOICES = (
-        ('public', 'Public'),
-        ('private', 'Private'),
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    is_recurring = models.BooleanField(default=False)
+    recurring_unit = models.CharField(
+        max_length=10,
+        choices=[('minutes', 'Minutes'), ('hours', 'Hours'), ('days', 'Days'), ('months', 'Months')],
+        blank=True,
+        null=True,
+        help_text="Interval between recurring games if applicable"
+    )
+    recurrence_interval = models.PositiveIntegerField(null=True, blank=True, help_text="Number of units between games")
+    
+    stake = models.DecimalField(max_digits=10, decimal_places=2)
+    is_public = models.BooleanField(default=True)  # public/private group
+
+    is_scheduled = models.BooleanField(default=False)  # whether to start at scheduled time
+    scheduled_datetime = models.DateTimeField(blank=True, null=True)
+
+    group_link = models.CharField(max_length=255, unique=True)
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='owned_groups'
     )
 
-    name = models.CharField(max_length=100, unique=True)
-    link = models.CharField(max_length=255, unique=True, blank=True, null=True)  # Store unique invitation link for private groups
-    privacy = models.CharField(max_length=7, choices=GROUP_PRIVACY_CHOICES, default='public')
-    owner = models.ForeignKey(AbstractUser, related_name='owned_groups', on_delete=models.CASCADE)
-    members = models.ManyToManyField(AbstractUser, related_name='group_members', through='GroupMembership')
-    
-    def save(self, *args, **kwargs):
-        if self.privacy == 'private' and not self.link:
-            # Generate a unique link if the group is private and doesn't already have a link
-            self.link = uuid.uuid4().hex
-        super().save(*args, **kwargs)
-    
+    subscribers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='subscribed_groups',
+        blank=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
-
-
-class GroupMembership(models.Model):
-    user = models.ForeignKey(AbstractUser, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    is_owner = models.BooleanField(default=False)
-
-    class Meta:
-        unique_together = ('user', 'group')
-
-    def __str__(self):
-        return f"{self.user} in {self.group}"
