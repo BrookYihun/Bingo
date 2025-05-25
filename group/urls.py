400b1b6b8931f24d38cfbb74791679f7d9193908
BrookYihun <brookyihun@gmail.com>
2025-05-25 10:40:14 +0300

    nm

--- group/urls.py
+++ group/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import GroupCreateUpdateView
 
 urlpatterns = [
-    path('/create-group', GroupCreateUpdateView.as_view(), name='group-create'),
+    path('/create-group/', GroupCreateUpdateView.as_view(), name='group-create'),
 ]
