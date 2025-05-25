8ddf8436590d9baf5406f9c31a59a42dc3c37b31
BrookYihun <brookyihun@gmail.com>
2025-05-25 11:08:37 +0300

    nmnm

--- group/urls.py
+++ group/urls.py
@@ -1,7 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import GroupCreateUpdateView
+from .views import GroupCreateUpdateView, my_groups, private_groups, public_groups
 
 urlpatterns = [
     path('create-group/', GroupCreateUpdateView.as_view(), name='group-create'),
+    path('my/',my_groups, name='my-groups'),
+    path('public/', public_groups, name='public-groups'),
+    path('private/', private_groups, name='private-groups'),
 ]
--- group/views.py
+++ group/views.py
@@ -2,6 +2,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import Group
 from .serializer import GroupSerializer
 from django.utils.crypto import get_random_string
@@ -28,5 +30,29 @@ class GroupCreateUpdateView(APIView):
         serializer = GroupSerializer(data=data)
         if serializer.is_valid():
             group = serializer.save()
+            group.subscribers.add(user)  # Auto-subscribe creator
             return Response(GroupSerializer(group).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def my_groups(request):
+    user = request.user
+    groups = Group.objects.filter(subscribers=user)
+    serializer = GroupSerializer(groups, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def public_groups(request):
+    groups = Group.objects.filter(is_public=True)
+    serializer = GroupSerializer(groups, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def private_groups(request):
+    user = request.user
+    groups = Group.objects.filter(is_public=False, subscribers=user)
+    serializer = GroupSerializer(groups, many=True)
+    return Response(serializer.data)
