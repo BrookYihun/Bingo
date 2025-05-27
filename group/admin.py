b376d1e2f27c154e4d17f801e52b3636b3ed0f48
BrookYihun <brookyihun@gmail.com>
2025-05-27 17:52:01 +0300

    nmnm

--- group/admin.py
+++ group/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from group.models import Group
+from group.models import Group, GroupGame
 
 # Register your models here.
 admin.site.register(Group)
+admin.site.register(GroupGame)
