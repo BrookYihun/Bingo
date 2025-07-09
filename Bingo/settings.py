40b129085cbfdce62b970c435899f91a93add63f
BrookYihun <brookyihun@gmail.com>
2025-07-09 20:14:17 +0300

    set

--- Bingo/settings.py
+++ Bingo/settings.py
@@ -165,6 +165,7 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [("127.0.0.1", 6379)],
+            "prefix": "dallol_bingo",
         },
     },
 }
