c3c46b0745b4b85b24315386970a7a04e185612c
BrookYihun <brookyihun@gmail.com>
2025-10-29 07:13:14 +0300

    bb

--- group/urls.py
+++ group/urls.py
@@ -1,6 +1,6 @@
 # urls.py
 from django.urls import path
-from .views import GroupCreateUpdateView, my_groups, private_groups, public_groups, subscribe_to_group, subscribe_via_referral, unsubscribe_from_group
+from .views import GroupCreateUpdateView, my_groups, owner_dashboard, private_groups, public_groups, request_group_withdrawal, subscribe_to_group, subscribe_via_referral, unsubscribe_from_group, withdrawal_history
 
 urlpatterns = [
     path('group/', GroupCreateUpdateView.as_view(), name='group-create'),
@@ -11,4 +11,7 @@ urlpatterns = [
     path("subscribe/", subscribe_to_group, name="subscribe-group"),
     path("unsubscribe/", unsubscribe_from_group, name="unsubscribe-group"),
     path("subscribe_referral/", subscribe_via_referral, name="subscribe-group-referral"),
+    path("dashboard/owner/",owner_dashboard, name="owner-dashboard"),
+    path("withdraw-history/",withdrawal_history, name="withdraw-history"),
+    path("withdraw-request/",request_group_withdrawal, name="withdraw-request"),
 ]
