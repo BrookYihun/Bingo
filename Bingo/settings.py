c6e8be6d51dd856d51d25d180bafcf6adca0846b
BrookYihun <brookyihun@gmail.com>
2025-12-17 13:59:16 +0300

    mm

--- Bingo/settings.py
+++ Bingo/settings.py
@@ -108,6 +108,26 @@ REST_FRAMEWORK = {
 
 ROOT_URLCONF = 'Bingo.urls'
 
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://3b9d9cc849194c0c490a99a63e77bb5d@o4510549827780608.ingest.de.sentry.io/4510549830074448",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    # Enable sending logs to Sentry
+    enable_logs=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profile_session_sample_rate to 1.0 to profile 100%
+    # of profile sessions.
+    profile_session_sample_rate=1.0,
+    # Set profile_lifecycle to "trace" to automatically
+    # run the profiler on when there is an active transaction
+    profile_lifecycle="trace",
+)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
